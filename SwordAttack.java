78b9ec21681660f69d5afa1032d2d824b013f83f
frankchan112358 <p0918727865@gmail.com>
2020-09-26 15:17:25 +0800

    m002

--- SwordAttack.java
+++ SwordAttack.java
@@ -1,7 +1,7 @@
 package HomeWork_ext_RoleGame;
 
 public class SwordAttack implements IAttackBehavior {
-	public float num;
+	public double num;
 	public void attack() {
 		System.out.println("攻擊");
 	}
